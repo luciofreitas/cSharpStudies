2e8a8fa428f87d85eea2031469aa0b3d7c591163
luciofreitas <luciodfp@gmail.com>
2023-01-23 12:49:52 -0300

    Exercicio

--- TempoJogo/TempoJogo/Program.cs
+++ TempoJogo/TempoJogo/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TempoJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal,duracaoJogo;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            horaFinal = int.Parse(vet[1]);
+
+            duracaoJogo = horaFinal - horaInicial; 
+            
+            if(horaInicial < horaFinal)
+            {
+                duracaoJogo = horaFinal - horaInicial;
+
+            }
+            else
+            {
+                duracaoJogo = 24 - horaInicial + horaFinal;
+            }
+            Console.WriteLine($"O jogo durou {duracaoJogo} horas");
+        }
+    }
+}
